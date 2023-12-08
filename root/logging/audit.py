fe3cba65706e4ce30f12139070acaf3ace1bdb5d
NareshThondapu <153311694+NareshThondapu@users.noreply.github.com>
2023-12-08 16:49:35 -0500

    Create audit.py

---
+++ root/logging/audit.py
@@ -0,0 +1,7 @@
+import logging
+
+# Initializing Logging  
+logging.getLogger("py4j").setLevel(logging.ERROR)
+logger.setLevel(logging.DEBUG)
+logging.captureWarnings(True)
+logger.info("********************************Execution started**************************************")
