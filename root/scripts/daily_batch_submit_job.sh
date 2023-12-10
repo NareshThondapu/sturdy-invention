204b9b64975a50947a336db372d39d6c1093d220
NareshThondapu <153311694+NareshThondapu@users.noreply.github.com>
2023-12-10 17:22:58 -0500

    Create daily_batch_submit_job.sh

---
+++ root/scripts/daily_batch_submit_job.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set Spark home directory
+SPARK_HOME=/user/programs/spark
+
+# Set application name
+APP_NAME=batch-app
+
+# Set HDFS directory containing the Spark application jar
+APP_JAR=/user/programs/batch-app.jar
+
+# Set HDFS directory for application logs
+LOG_DIR=/user/data/logs/batch-app
+
+# Define date for today
+TODAY=$(date -d "today" +%Y-%m-%d)
+
+# Build the Spark submit command
+spark_submit_cmd="$SPARK_HOME/bin/spark-submit \
+    --master yarn \
+    --name $APP_NAME \
+    --deploy-mode cluster \
+    --driver-memory 1g \
+    --executor-memory 2g \
+    --num-executors 4 \
+    --files /path/to/configuration.yaml \
+    --class com.example.batch-app \
+    $APP_JAR $TODAY $LOG_DIR"
+
+# Run the Spark application
+eval $spark_submit_cmd
